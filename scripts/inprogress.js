3db588def114d1e046c4bad2dcb8fdf40252445d
tranteresa <tranteresaat@gmail.com>
2018-03-24 01:29:10 -0700

    Adding content for html, css, and setting up js

---
+++ scripts/inprogress.js
@@ -0,0 +1,38 @@
+$("#cssmenu").menumaker({
+    title: "Menu",
+    breakpoint: 768,
+    format: "multitoggle"
+});
+
+$("#cssmenu").prepend("<div id='menu-line'></div>");
+
+var foundActive = false, activeElement, linePosition = 0, menuLine = $("#cssmenu #menu-line"), lineWidth, defaultPosition, defaultWidth;
+
+$("#cssmenu > ul > li").each(function() {
+  if ($(this).hasClass('active')) {
+    activeElement = $(this);
+    foundActive = true;
+  }
+});
+
+if (foundActive === false) {
+  activeElement = $("#cssmenu > ul > li").first();
+}
+
+defaultWidth = lineWidth = activeElement.width();
+
+defaultPosition = linePosition = activeElement.position().left;
+
+menuLine.css("width", lineWidth);
+menuLine.css("left", linePosition);
+
+$("#cssmenu > ul > li").hover(function() {
+  activeElement = $(this);
+  lineWidth = activeElement.width();
+  linePosition = activeElement.position().left;
+  menuLine.css("width", lineWidth);
+  menuLine.css("left", linePosition);
+}, function() {
+  menuLine.css("left", defaultPosition);
+  menuLine.css("width", defaultWidth);
+});
